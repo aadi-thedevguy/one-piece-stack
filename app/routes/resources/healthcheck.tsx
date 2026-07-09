53942c54eee05101c58db9c5546d848556f09a31
Aditya khare <adikhare24@gmail.com>
2026-07-09 09:21:02 +0530

    fix(server): healthcheck endpoint

--- app/routes/resources/healthcheck.tsx
+++ app/routes/resources/healthcheck.tsx
@@ -1,24 +1,9 @@
-// learn more: https://fly.io/docs/reference/configuration/#services-http_checks
-
 import { prisma } from "~/lib/db.server";
-import type { Route } from "./+types/healthcheck";
 
-export async function loader({ request }: Route.LoaderArgs) {
-  const host =
-    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
-
+export async function loader() {
   try {
     // if we can connect to the database and make a simple query
-    // and make a HEAD request to ourselves, then we're good.
-    await Promise.all([
-      prisma.user.count(),
-      fetch(`${new URL(request.url).protocol}${host}`, {
-        method: "HEAD",
-        headers: { "X-Healthcheck": "true" },
-      }).then((r) => {
-        if (!r.ok) return Promise.reject(r);
-      }),
-    ]);
+    await prisma.user.count();
     return new Response("OK");
   } catch (error: unknown) {
     console.log("healthcheck ❌", { error });
